f92faea4ed2dc51413f8209625e2de13e87556c3
dj1sklero <60883106+dj1sklero@users.noreply.github.com>
2020-03-19 21:04:02 +0100

    Initial commit

---
+++ src/main/java/pl/dj1sklero/zombies/Zombies.java
@@ -0,0 +1,17 @@
+package pl.dj1sklero.zombies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Zombies extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
