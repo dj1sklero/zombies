a906510cec7796abc2191d8657836ded670dfc8a
dj1sklero <60883106+dj1sklero@users.noreply.github.com>
2020-03-19 21:30:00 +0100

    update

--- src/main/java/pl/dj1sklero/zombies/Zombies.java
+++ src/main/java/pl/dj1sklero/zombies/Zombies.java
@@ -2,16 +2,18 @@ package pl.dj1sklero.zombies;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+
 public final class Zombies extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
